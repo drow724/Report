d16d7282111055b669b3653c77ae62ff61177a95
drow724 <drow724@naver.com>
2025-02-27 10:50:41 +0900

    kb fund bug fix

--- src/main/java/com/report/service/KBPortfolioService.java
+++ src/main/java/com/report/service/KBPortfolioService.java
@@ -18,7 +18,7 @@ public class KBPortfolioService {
 
     public void generateKBPortfolio() {
         try (Playwright playwright = Playwright.create();
-             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false))) {
+             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions())) {
 
             Page page = browser.newPage();
             page.navigate("https://obank.kbstar.com/quics?page=C055068&QSL=F#loading");
