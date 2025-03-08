4542d3533641fcfa13aa3a52355793d59f5445f9
drow724 <drow724@naver.com>
2025-03-08 17:15:09 +0900

    remove unused code

--- src/main/java/com/report/container/KBPortfolioCacheContainer.java
+++ src/main/java/com/report/container/KBPortfolioCacheContainer.java
@@ -12,20 +12,7 @@ public class KBPortfolioCacheContainer {
 
     private KBPortfolioDTO dto = new KBPortfolioDTO();
 
-
-    private String jsessionId = "0000dAolXvxd-J-d7nKlo6qnSQD:BANK10904";
-
-    private String qsid = "19A4&&_9PD36NJrqwlJlHVFVTxZOG";
-
     synchronized public void setDto(KBPortfolioDTO dto) {
         this.dto = dto;
-    }
-
-    synchronized public void setJsessionId(String jsessionId) {
-        this.jsessionId = jsessionId;
-    }
-
-    synchronized public void setQsid(String qsid) {
-        this.qsid = qsid;
     }
 }
